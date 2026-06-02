35da7d8abb819d74c2e93eb9c9bb0a614b37c251
Ridy Lie <eeyore@gmail.com>
2026-06-02 10:00:45 +0700

    regex json minor fix

--- src/utils/json.ts
+++ src/utils/json.ts
@@ -1,7 +1,9 @@
 export function extractJson(text: string) {
-  return text
-    .replace(/```json/g, '')
-    .replace(/```/g, '')
-    .replace(/(?<!\\)\\(?!["\\\/bfnrtu])/g, '\\\\')
-    .trim()
+  return (
+    text
+      .replace(/```json/g, '')
+      .replace(/```/g, '')
+      //    .replace(/(?<!\\)\\(?!["\\\/bfnrtu])/g, '\\\\')
+      .trim()
+  )
 }
