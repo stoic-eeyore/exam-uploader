5dcce73536a499d70e9501d51fc625d1e8ad3184
Ridy Lie <eeyore@gmail.com>
2026-08-02 01:07:24 +0700

    minor cron fix

--- src/app/api/cron/analyze-pending-exam/route.ts
+++ src/app/api/cron/analyze-pending-exam/route.ts
@@ -25,7 +25,7 @@ export async function GET(req: NextRequest) {
         },
         {
           aiAnalysis: {
-            not_equals: null,
+            equals: null,
           },
         },
       ],
